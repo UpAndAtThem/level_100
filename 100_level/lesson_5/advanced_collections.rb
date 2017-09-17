41fa56d8641d0633cf91d09f18de119c2b7362ad
Jason Nelson <Jasonn318@gmail.com>
2017-09-17 09:26:25 -0500

    added problem solution that was forgotten

--- 100_level/lesson_5/advanced_collections.rb
+++ 100_level/lesson_5/advanced_collections.rb
@@ -38,6 +38,26 @@
 # p hsh2[:third].key(0)
 # -------------------------------------------------
 
+#For each of these collection objects where the value 3 occurs, demonstrate how you would change this to 4.
+
+# arr1 = [1, [2, 3], 4]
+
+# arr2 = [{a: 1}, {b: 2, c: [7, 6, 5], d: 4}, 3]
+
+# hsh1 = {first: [1, 2, [3]]}
+
+# hsh2 = {['a'] => {a: ['1', :two, 3], b: 4}, 'b' => 5}
+
+
+# arr1[1][1] = 4
+
+# arr2[2] = 4
+
+# hsh1[:first][2][0] = 4
+
+# hsh2[['a']][:a][2] = 4
+# -------------------------------------------------
+
 # find sum of all the male ages
 # munsters = {
 #   "Herman" => { "age" => 32, "gender" => "male" },
