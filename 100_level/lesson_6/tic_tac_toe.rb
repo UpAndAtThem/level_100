79cd63c783bbdc1264370fe910dce9b368e084c4
Jason Nelson <Jasonn318@gmail.com>
2018-05-16 20:44:50 -0500

    refactored play_again?, join_or and rubocop refactoring.

--- 100_level/lesson_6/tic_tac_toe.rb
+++ 100_level/lesson_6/tic_tac_toe.rb
@@ -1,9 +1,7 @@
-require 'pry'
-
 INITIAL_MARKER = ' '.freeze
 PLAYER_MARKER = 'X'.freeze
 COMPUTER_MARKER = 'O'.freeze
-GOES_FIRST = 'choose'
+GOES_FIRST = 'choose'.freeze
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]] # diagnals
@@ -28,9 +26,7 @@ end
 # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def initalize_board
-  new_board = {}
-  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
-  new_board
+  (1..9).each_with_object({}) { |num, board| board[num] = INITIAL_MARKER }
 end
 
 def empty_squares(brd)
@@ -41,41 +37,46 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def player_places_piece(brd)
+def player_places_piece!(brd)
   square = ''
+
   loop do
-    prompt "Choose a position to place a piece: #{joinor(empty_squares(brd))}"
+    remaining = joinor(empty_squares(brd), ',', 'or')
+    prompt "Choose a position to place a piece: #{remaining}"
     square = gets.chomp.to_i
+
     break if empty_squares(brd).include?(square)
     prompt 'Sorry, thats not a valid choice'
   end
+
   brd[square] = PLAYER_MARKER
   display_board brd
 end
 
-def computer_places_piece(brd)
+# rubocop:disable Metrics/MethodLength
+def computer_places_piece!(brd)
   sleep 0.5
+
   if offensive_move? brd
-    square = offensive_move(brd)
-    brd[square] = 'O'
+    brd[offensive_move(brd)] = 'O'
   elsif defensive_move? brd
-    square = defensive_move(brd)
-    brd[square] = 'O'
+    brd[defensive_move(brd)] = 'O'
   elsif brd[5] == ' '
     brd[5] = 'O'
   else
-    square = empty_squares(brd).sample
-    brd[square] = COMPUTER_MARKER
+    brd[empty_squares(brd).sample] = COMPUTER_MARKER
   end
+
   display_board brd
 end
+# rubocop:enable Metrics/MethodLength
 
 def board_full?(brd)
   empty_squares(brd) == []
 end
 
 def someone_won?(brd)
-  !!detect_winner(brd)
+  detect_winner(brd).nil? ? false : true
 end
 
 def detect_winner(brd)
@@ -87,111 +88,123 @@ def detect_winner(brd)
 end
 
 def joinor(arr, symbol = ',', conjunction = 'or')
-  result = ''
-  arr.each_with_index do |square, index|
-    result << "#{square}#{symbol} " if index + 1 != arr.length
+  arr.each_with_object('').with_index do |(square, result), index|
+    result << "#{square}#{symbol} " unless index + 1 == arr.length
     result << "#{conjunction} #{square}" if index + 1 == arr.length
   end
-  result
 end
 
 def play_again?
-  play_again = ''
   loop do
     prompt 'Would you like to play again? (Y/N)'
     play_again = gets.chomp.downcase
-    play_again = play_again == 'y' ? true : false # if is_true
-    break
+
+    return play_again if %w(y Y yes Yes YES n N no No NO).include? play_again
   end
-  play_again
 end
 
 def defensive_move?(brd)
   WINNING_LINES.each do |line|
-    return true if brd.values_at(*line).count('X') == 2 && brd.values_at(*line).count(' ') == 1
+    return true if brd.values_at(*line).count('X') == 2 &&
+                   brd.values_at(*line).count(' ') == 1
   end
+
   false
 end
 
 def defensive_move(brd)
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count('X') == 2 && brd.values_at(*line).count(' ') == 1
-      line.select{ |square| return square if empty_squares(brd).include? square }
+    if brd.values_at(*line).count('X') == 2 &&
+       brd.values_at(*line).count(' ') == 1
+
+      line.select do |square|
+        return square if empty_squares(brd).include? square
+      end
     end
+
+    next
   end
   nil
 end
 
 def offensive_move?(brd)
   WINNING_LINES.each do |line|
-    return true if brd.values_at(*line).count('O') == 2 && brd.values_at(*line).count(' ') == 1
+    return true if brd.values_at(*line).count('O') == 2 &&
+                   brd.values_at(*line).count(' ') == 1
   end
   false
 end
 
 def offensive_move(brd)
   WINNING_LINES.each do |line|
-    if brd.values_at(*line).count('O') == 2 && brd.values_at(*line).count(' ') == 1
-      line.select{ |square| return square if empty_squares(brd).include? square }
+    if brd.values_at(*line).count('O') == 2 &&
+       brd.values_at(*line).count(' ') == 1
+      line.select do |square|
+        return square if empty_squares(brd).include? square
+      end
     end
+
+    next
   end
   nil
 end
 
 def place_piece!(brd, current_player)
-  player_places_piece brd if current_player == 'player'
-  computer_places_piece brd if current_player == 'computer'
-  return
+  player_places_piece! brd if current_player == 'player'
+  computer_places_piece! brd if current_player == 'computer'
+  nil
 end
 
 def alternate_player(current_player)
-  return 'computer' if current_player == 'player'
-  return 'player' if current_player == 'computer'
+  current_player == 'player' ? 'computer' : 'player'
 end
 
-def choose_first
-  first_player = ''
+def choose_who_goes_first
   loop do
-    prompt"Who goes first? Choose player or computer"
+    prompt 'Who goes first? Choose "player" or "computer"'
     first_player = gets.chomp
-    return first_player if ['computer', 'player'].include? first_player
-    prompt "That is not a valid choice"
+
+    return first_player if %w(computer player).include? first_player
+    prompt 'That is not a valid choice'
   end
 end
 
 computer_score = 0
 player_score = 0
 first_player = GOES_FIRST
-first_player = choose_first if GOES_FIRST == 'choose'
+first_player = choose_who_goes_first if GOES_FIRST == 'choose'
 current_player = first_player
 
 loop do
   board = initalize_board
-  winner = ''
   current_player = first_player
 
   loop do
     display_board(board)
     prompt "player: #{player_score} | computer: #{computer_score}"
+
     place_piece!(board, current_player)
     current_player = alternate_player(current_player)
+
     break if someone_won?(board) || board_full?(board)
   end
 
   winner = detect_winner(board)
   player_score += 1 if winner == 'Player'
   computer_score += 1 if winner == 'Computer'
-  
+
   if player_score == 5 || computer_score == 5
     prompt "#{detect_winner(board)} won 5 times, and is the overall winner!"
+
     player_score = 0
-    computer_score = 0 
-    break if play_again? == false
+    computer_score = 0
+
+    break if %w(n N no No NO).include? play_again?
   elsif someone_won?(board)
     prompt "#{detect_winner(board)} won!"
   else
     prompt 'It\'s a tie!'
   end
-  sleep 0.85
 
+  sleep 0.90
 end
