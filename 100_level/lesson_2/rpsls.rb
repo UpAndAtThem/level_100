5e0e0c44624e1481bd14da3db12edc4cf446b78f
Jason Nelson <Jasonn318@gmail.com>
2018-05-31 23:44:28 -0500

    refactored rpsls.rb

--- 100_level/lesson_2/rpsls.rb
+++ 100_level/lesson_2/rpsls.rb
@@ -1,15 +1,21 @@
-VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
+require 'yaml'
+
+VALID_CHOICES = {rock: '1', paper: '2', scissors: '3',
+                 lizard: '4', spock: '5'}.freeze
+
 
 WINNING_HAND = { rock: ['lizard', 'scissors'], paper: ['rock', 'spock'],
                  scissors: ['lizard', 'paper'], lizard: ['spock', 'paper'],
                  spock: ['rock', 'scissors'] }.freeze
 
+MESSAGES = YAML.load_file('rpsls_messages.yml')
+
 def prompt(message)
-  puts "\n=> #{message}"
+  puts "=> #{message}"
 end
 
-def display_winner(player, computer)
-  if WINNING_HAND[player.to_sym].include? computer
+def win_lose_tie(player, computer)
+  if WINNING_HAND[player].include? computer.to_s
     'you win!'
   elsif player == computer
     'It is a tie!'
@@ -18,12 +24,36 @@ def display_winner(player, computer)
   end
 end
 
-def valid_entry?(choice)
-  VALID_CHOICES.include?(choice)
+def valid_choice?(choice)
+  VALID_CHOICES.values.include?(choice)
 end
 
-def tally_score(winner)
+def increment_score(winner)
   winner += 1
+end
+
+def choose_rpsls()
+  loop do
+    prompt MESSAGES['choose_prompt']
+    print "\n==> " 
+    player_choice = gets.chomp
+
+    return VALID_CHOICES.key(player_choice) if valid_choice?(player_choice)
+    prompt MESSAGES['error_choice']
+  end
+end
+
+def winner?(player_choice, computer_choice)
+  WINNING_HAND[player_choice].include? computer_choice.to_s
+end
+
+def display_result_and_score(p_score, p_choice, c_score, c_choice)
+  system"clear"
+  prompt "you have #{p_choice.upcase}, the computer has #{c_choice.upcase}. " + 
+         win_lose_tie(p_choice, c_choice) + "\n\n"
+  prompt "player score: #{p_score}"
+  prompt "computer_score: #{c_score}\n\n"
+
 end
 
 player_score = 0
@@ -32,26 +62,25 @@ player_choice = ''
 computer_choice = ''
 
 loop do
-  loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    player_choice = gets.chomp
-    break if valid_entry? player_choice
-    prompt "you need to enter one: #{VALID_CHOICES.join(', ')}"
+  system "clear"
+
+  player_choice = choose_rpsls
+  computer_choice = VALID_CHOICES.keys.sample
+
+  if winner?(player_choice, computer_choice)
+    player_score = increment_score player_score
+  elsif winner?(computer_choice, player_choice) 
+    computer_score = increment_score computer_score
   end
 
-  computer_choice = VALID_CHOICES.sample
+  display_result_and_score(player_score, player_choice, computer_score, computer_choice)
 
-  prompt "you have #{player_choice}, and the computer has #{computer_choice}." + (display_winner player_choice, computer_choice)
-
-  if WINNING_HAND[player_choice.to_sym].include? computer_choice
-    player_score = tally_score player_score
-  elsif player_choice == computer_choice
-    next
-  else computer_score = tally_score computer_score
-  end
+  prompt MESSAGES['press_enter'] 
+  gets
 
   if player_score == 5 || computer_score == 5
-    prompt "GAME OVER! You won #{player_score} #{player_score == 1 ? 'time' : 'times'}, and the computer won #{computer_score} #{computer_score == 1 ? 'time' : 'times'}"
+    prompt "GAME OVER! You won #{player_score} #{player_score == 1 ? 'time' : 'times'}" +
+            ", and the computer won #{computer_score} #{computer_score == 1 ? 'time' : 'times'}"
     break
   end
 end
