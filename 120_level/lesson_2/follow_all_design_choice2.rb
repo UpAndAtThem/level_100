cf0ea0c992f7b3bf91cbf5decb2af1a63c6f4929
Jason Nelson <Jasonn318@gmail.com>
2018-07-12 18:30:44 -0500

    reordered the class list

--- 120_level/lesson_2/follow_all_design_choice2.rb
+++ 120_level/lesson_2/follow_all_design_choice2.rb
@@ -1,62 +1,4 @@
 require 'pry'
-class RPSGame
-  attr_accessor :human, :computer
-
-  def initialize
-    @human = Human.new
-    @computer = Computer.new()
-  end
-
-  def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
-  end
-
-  def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors!"
-  end
-
-  def display_winner
-    puts "#{human.name} chose #{human.move.value}."
-    puts "#{computer.name} chose #{computer.move.value}"
-
-    if human.move > computer.move
-      puts "#{human.name} won!"
-    elsif human.move < computer.move
-      puts "#{computer.name} won!"
-    else
-      puts "It's a tie!"
-    end
-  end
-
-  def play_again?
-    choice = nil
-
-    loop do
-      puts "would you like to play again? (y/n)"
-      choice = gets.chomp
-      break if %w(y n).include? choice 
-      puts "enter 'y' or 'n'"
-    end
-
-    return true if choice == 'y'
-    return false
-  end
-
-  def play
-    display_welcome_message
-
-    loop do
-      human.choose
-      computer.choose
-
-      display_winner
-
-      break unless play_again?
-    end
-
-    display_goodbye_message
-  end
-end
 
 class Player
   attr_accessor :move, :name
@@ -69,12 +11,12 @@ end
 class Human < Player
   def choose
     choice = nil
-      loop do
-        puts "Choose rock, paper, or scissors"
-        choice = gets.chomp
-        break if Move::VALUES.include? choice
-        puts "Sorry, invalid choice."
-      end
+    loop do
+      puts "Choose rock, paper, or scissors"
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts "Sorry, invalid choice."
+    end
 
     self.move = Move.new(choice)
   end
@@ -128,13 +70,13 @@ class Move
     case @value
     when 'rock'
       return true if other_move.scissors?
-      return false
+      false
     when 'paper'
       return true if other_move.rock?
-      return false
+      false
     when 'scissors'
       return true if other_move.paper?
-      return false
+      false
     end
   end
 
@@ -142,14 +84,73 @@ class Move
     case @value
     when 'rock'
       return true if other_move.paper?
-      return false
+      false
     when 'paper'
       return true if other_move.scissors?
-      return false
+      false
     when 'scissors'
       return true if other_move.rock?
-      return false
+      false
     end
+  end
+end
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new()
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors!"
+  end
+
+  def display_winner
+    puts "#{human.name} chose #{human.move.value}."
+    puts "#{computer.name} chose #{computer.move.value}"
+
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    choice = nil
+
+    loop do
+      puts 'would you like to play again? (y/n)'
+      choice = gets.chomp
+      break if ['y', 'n'].include? choice
+      puts "enter 'y' or 'n'"
+    end
+
+    return true if choice == 'y'
+    false
+  end
+
+  def play
+    display_welcome_message
+
+    loop do
+      human.choose
+      computer.choose
+
+      display_winner
+
+      break unless play_again?
+    end
+
+    display_goodbye_message
   end
 end
 
@@ -160,8 +161,6 @@ class Rule
 end
 
 # not sure where "compare" goes yet
-def compare(move1, move2)
-
-end
+def compare(move1, move2); end
 
 RPSGame.new.play
