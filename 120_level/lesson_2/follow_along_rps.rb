c5e6017ccce76c010e66ebb14c04ef93967e460c
Jason Nelson <Jasonn318@gmail.com>
2018-07-19 23:41:19 -0500

    add follow_along_rps.rb

--- 120_level/lesson_2/follow_along_rps.rb
+++ 120_level/lesson_2/follow_along_rps.rb
@@ -2,8 +2,8 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new(:computer)
+    @human = Human.new
+    @computer = Computer.new()
   end
 
   def display_welcome_message
@@ -50,12 +50,16 @@ class RPSGame
 
   def play
     display_welcome_message
+    
     loop do
       human.choose
       computer.choose
+
       display_winner
+
       break unless play_again?
     end
+
     display_goodbye_message
   end
 end
@@ -63,44 +67,45 @@ end
 class Player
   attr_accessor :move, :name
 
-  def initialize(player_type = :human)
-    @player_type = player_type
-    @move = nil
+  def initialize
     set_name
   end
+end
 
-  def set_name
-    if human?
-      n = ""
-      loop do
-        puts "What's your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must have input"
-      end
-      self.name = n
-    else
-      self.name = ['R2D2', 'Hal', 'Chappie', 'Dolores'].sample
-    end
-  end
-
+class Human < Player
   def choose
-    if human?
-      choice = nil
+    choice = nil
       loop do
         puts "Choose rock, paper, or scissors"
         choice = gets.chomp
         break if %w(rock paper scissors).include? choice
         puts "Sorry, invalid choice."
       end
-      self.move = choice
-    else
-      self.move = %w(rock paper scissors).sample
-    end
+
+    self.move = choice
   end
 
-  def human?
-    @player_type == :human
+  def set_name
+    n = ""
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must have input"
+    end
+    self.name = n
+  end
+end
+
+class Computer < Player
+  attr_accessor :name
+
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Dolores'].sample
+  end
+
+  def choose
+    self.move = %w(rock paper scissors).sample
   end
 end
 
