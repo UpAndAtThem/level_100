c9a931152cc226f8325427a16f758044c70b930e
Jason Nelson <Jasonn318@gmail.com>
2019-01-03 14:46:52 -0600

    add server.rb

---
+++ 170_level/lesson_1/server.rb
@@ -0,0 +1,22 @@
+require "socket"
+
+server = TCPServer.new("localhost", 3003)
+
+loop do
+  client = server.accept
+  first_request_line = client.gets
+
+  http_method, path_and_params, http = first_request_line.split(" ")
+  params = path_and_params.scan(/(\w+)=(\w+)/).to_h
+  path = path_and_params.match(/\/\w+(?=\?*)/).to_s
+
+  client.puts "HTTP/1.1 200 OK\r\n\r\n"
+  client.puts "method : #{http_method}"
+  client.puts "http : #{http}"
+  client.puts "params : #{params}"
+  client.puts "path_and_params : #{path_and_params}"
+  client.puts "path : #{path}"
+
+  client.close
+  break
+end
