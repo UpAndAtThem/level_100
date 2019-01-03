22ac5db56776c7d077e9070722457e0b7cf52988
Jason Nelson <Jasonn318@gmail.com>
2019-01-03 15:56:52 -0600

    refactored path regex to accept multiple directories

--- 170_level/lesson_1/server.rb
+++ 170_level/lesson_1/server.rb
@@ -7,8 +7,9 @@ loop do
   first_request_line = client.gets
 
   http_method, path_and_params, http = first_request_line.split(" ")
+
   params = path_and_params.scan(/(\w+)=(\w+)/).to_h
-  path = path_and_params.match(/\/\w+(?=\?*)/).to_s
+  path = path_and_params.match(/(\/\w+){0,}(?=\?*)/).to_s
 
   client.puts "HTTP/1.1 200 OK\r\n\r\n"
   client.puts "method : #{http_method}"
