d9f9479c5453190136bd47a3a5dcb2f84c4148d7
Jason Nelson <Jasonn318@gmail.com>
2019-01-03 18:48:20 -0600

    fixed bug with the next if not first_line || first_line has favicon

--- 170_level/lesson_1/server.rb
+++ 170_level/lesson_1/server.rb
@@ -1,15 +1,39 @@
 require "socket"
+require 'pry'
 
 server = TCPServer.new("localhost", 3003)
 
+class Dice
+  attr_reader :sides
+
+  def initialize(sides)
+    @sides = sides
+  end
+
+  def roll
+    rand(sides) + 1
+  end
+end
+
 loop do
   client = server.accept
-  first_request_line = client.gets
+  first_line = client.gets
 
-  http_method, path_and_params, http = first_request_line.split(" ")
+  next if !first_line || first_line =~ /favicon/
 
-  params = path_and_params.scan(/(\w+)=(\w+)/).to_h
+  http_method, path_and_params, http = first_line.split(" ")
+
+  params = path_and_params.scan(/(\w+)=(\w+)/)
+                          .map { |k,v| [k, v.to_i]}
+                          .to_h
+
   path = path_and_params.match(/(\/\w+){0,}(?=\?*)/).to_s
+
+  rolls = params['rolls']
+  sides = params['sides']
+  dice = Dice.new(sides)
+
+  sum_of_rolls = rolls.times.reduce(0){ |memo, _| memo += dice.roll }
 
   client.puts "HTTP/1.1 200 OK\r\n\r\n"
   client.puts "method : #{http_method}"
@@ -17,6 +41,10 @@ loop do
   client.puts "params : #{params}"
   client.puts "path_and_params : #{path_and_params}"
   client.puts "path : #{path}"
+  client.puts ""
+  client.puts "number_of_rolls : #{rolls}"
+  client.puts "sides_on_dice : #{sides}"
+  client.puts "sum_of_rolls : #{sum_of_rolls}"
 
   client.close
 end
